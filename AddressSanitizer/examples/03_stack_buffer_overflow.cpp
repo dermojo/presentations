7cb42af83f6ea34d4271dcc2064e83223105ba51
Daniel Evers <dermojo@gmail.com>
2018-10-15 20:16:29 +0200

    replacing 'const char*' with 'std::string' to ensure data on the stack

--- AddressSanitizer/examples/03_stack_buffer_overflow.cpp
+++ AddressSanitizer/examples/03_stack_buffer_overflow.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char** argv)
 {
-    std::array<const char*, 3> data{ "no", "one", "two" };
+    std::array<std::string, 3> data{ "no", "one", "two" };
     std::cout << "You passed " << data[argc - 1] << " args(s)\n";
     return 0;
 }
